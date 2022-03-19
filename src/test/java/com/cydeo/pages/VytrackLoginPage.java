df4e5dec3fcd146306144a23475d32e53f65e244
vlatkapersin <vlatka.persin@gmail.com>
2022-03-19 19:35:07 -0400

    AvengersCucumber setup

---
+++ src/test/java/com/cydeo/pages/VytrackLoginPage.java
@@ -0,0 +1,22 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VytrackLoginPage {
+
+    public VytrackLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "prependedInput")
+    public WebElement userInput;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement passwordInput;
+
+    @FindBy(id = "_submit")
+    public WebElement loginButton;
+}
